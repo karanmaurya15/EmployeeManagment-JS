fd157aa8e4b5d190b96f6f9cece385835b0477d2
karanmaurya15 <karanmaurya9663@gmail.com>
2023-01-18 12:16:43 +0530

    [Karan] Employee Absent or Present

---
+++ EmployeewageManagment/EmployeeWageProgram.js
@@ -0,0 +1,9 @@
+//Welcome to Employee Wage Problem
+//UC1
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) {
+  console.log("UC1--- Employee is Absent");
+} else {
+  console.log("UC1--- Employee is Present");
+}
